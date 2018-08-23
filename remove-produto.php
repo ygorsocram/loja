8dc6eb66d1f556db340c5efa6fb305e360675b7b
Ygor Batista <ygor.batista@grupocompeti.com.br>
2018-08-23 16:15:20 -0300

    aula_final

--- remove-produto.php
+++ remove-produto.php
@@ -1,10 +1,10 @@
-<?php include("cabecalho.php");
-      include("conecta.php");
-      include("banco-produto.php");
+<?php include("cabecalho.php"); 		
+ include("conecta.php");			
+ include("banco-produto.php"); 
 
-$id = $_POST['id'];
-removeProduto($conexao, $id);
-
-header("Location: produto-lista.php?removido=true");
-die();
-?>
+ $id = $_POST['id'];
+ removeProduto($conexao, $id);
+ header("Location: produto-lista.php?removido=true");
+ die();
+ ?>
+ 
