a2f277957b593b67439abd29bdec7dc2ef749c53
Ygor Batista <ygor.batista@grupocompeti.com.br>
2018-08-25 10:00:04 -0300

    aula 7

--- produto-formulario.php
+++ produto-formulario.php
@@ -5,38 +5,18 @@
 
 verificaUsuario();
 
+$produto = array("nome" => "", "descricao" => "", "preco" => "", "categoria_id" => "1");
+$usado = "";
+
 $categorias = listaCategorias($conexao);
 ?>
 
 <h1>Formulário de produto</h1>
 <form action="adiciona-produto.php" method="post">
     <table class="table">
-        <tr>
-            <td>Nome</td>
-            <td><input class="form-control" type="text" name="nome" /></td>
-        </tr>
-        <tr>
-            <td>Preço</td>
-            <td><input class="form-control" type="number" name="preco" /></td>
-        </tr>
-        <tr>
-            <td>Descrição</td>
-            <td><textarea class="form-control" name="descricao"></textarea></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td><input type="checkbox" name="usado" value="true"> Usado
-        </tr>
-        <tr>
-            <td>Categoria</td>
-            <td>
-                <select class="form-control" name="categoria_id">
-                    <?php foreach($categorias as $categoria) : ?>
-                        <option value="<?=$categoria['id']?>"><?=$categoria['nome']?></option>
-                    <?php endforeach ?>
-                </select>
-            </td>
-        </tr>
+
+        <?php include("produto-formulario-base.php"); ?>
+        
         <tr>
             <td><button class="btn btn-primary" type="submit">Cadastrar</button></td>
         </tr>
